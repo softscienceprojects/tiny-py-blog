c10a89b8e1ad8c9d3561af7f3b872436533abf3c
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-01-26 13:40:09 +0000

    changed to template render

--- controller.py
+++ controller.py
@@ -4,13 +4,14 @@ urls = (
     '/', 'home'
 )
 
+render = web.template.render("Views/Templates", base="mainlayout")
 app = web.application(urls, globals())
 
 # classes/routes
 
 class home:
     def GET(self):
-        return "home"
+        return render.home()
 
 if __name__ == "__main__":
     app.run()
