787f10fb8a92dda75ed54ca2edd1cb5de618e5eb
erin nicole johnson <erin.nicole.johnson@gmail.com>
2020-01-26 15:27:30 +0000

    added __init__

--- models/RegisterModel.py
+++ models/RegisterModel.py
@@ -2,5 +2,10 @@ import pymongo
 from pymongo import MongoClient
 
 class RegisterModel:
+
+    def __init__(self):
+        self.client = MongoClient()
+       # self.db = self.client. #need to register!
+
     def insert_user(self, data):
         print("data is ", data)
